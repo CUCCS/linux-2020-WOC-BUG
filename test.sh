7b65a9871b375d5b1388e113fbed64eb77dfdc65
WOC-BUG <1539777243@qq.com>
2020-05-12 13:43:41 +0800

    change output

--- test.sh
+++ test.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-for file in ./0x04/*.sh;do
-        if [[ $file =~ $0 ]];then
-                continue
-        fi
-        printf "==================== %s ====================\n" "$file"
-        bash "$file"
-	printf "-------------------- %s --------------------\n" "$file"
-	shellcheck "$file"
-done
+printf "==================== task1 ====================\n"
+bash 0x04/ImageProcessing.sh -h
+bash 0x04/ImageProcessing.sh -f img/spring.jpeg -q      # 压缩jpg质量
+bash 0x04/ImageProcessing.sh -f img/boy.png -r          # 压缩分辨率
+bash 0x04/ImageProcessing.sh -f img/xumo.svg -t         # 格式转为jpg
+bash 0x04/ImageProcessing.sh -f img/girl.png -p prefix  # 添加前缀
+bash 0x04/ImageProcessing.sh -f img -m                  # 批量添加水印
+printf "-------------------- shellcheck --------------------"
+shellcheck 0x04/ImageProcessing.sh
+
+printf "==================== task2 ====================\n"
+bash 0x04/WorldCup.sh
+printf "-------------------- shellcheck --------------------"
+shellcheck 0x04/WorldCup.sh
+
+printf "==================== task3 ====================\n"
+cat result_task3.txt
