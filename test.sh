f3d17b5c261cc4cb159ac27808d112e2cc777dd2
WOC-BUG <1539777243@qq.com>
2020-05-12 14:19:52 +0800

    change watermark path

--- test.sh
+++ test.sh
@@ -2,11 +2,11 @@
 
 printf "==================== task1 ====================\n"
 bash 0x04/ImageProcessing.sh -h
-bash 0x04/ImageProcessing.sh -f 0x04/img/spring.jpeg -q 50   # 压缩jpg质量
-bash 0x04/ImageProcessing.sh -f 0x04/img/boy.png -r 100      # 压缩分辨率
-bash 0x04/ImageProcessing.sh -c 0x04/img/xumo.svg            # 格式转为jpg
-bash 0x04/ImageProcessing.sh -f 0x04/img/girl.png -p prefix  # 添加前缀
-bash 0x04/ImageProcessing.sh -m 0x04/img                     # 批量添加水印
+bash 0x04/ImageProcessing.sh -f 0x04/img/spring.jpeg -q 50   	# 压缩jpg质量
+bash 0x04/ImageProcessing.sh -f 0x04/img/boy.png -r 100      	# 压缩分辨率
+bash 0x04/ImageProcessing.sh -c 0x04/img/xumo.svg            	# 格式转为jpg
+bash 0x04/ImageProcessing.sh -f 0x04/img/girl.png -p prefix  	# 添加前缀
+bash 0x04/ImageProcessing.sh -m 0x04/img 0x04/img/watermark.png	# 批量添加水印
 printf "-------------------- shellcheck --------------------"
 shellcheck 0x04/ImageProcessing.sh
 
