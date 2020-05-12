7d75c422a95f963d26ae3b1f00dd8446260a7756
WOC-BUG <1539777243@qq.com>
2020-05-12 13:46:40 +0800

    change file path

--- test.sh
+++ test.sh
@@ -2,11 +2,11 @@
 
 printf "==================== task1 ====================\n"
 bash 0x04/ImageProcessing.sh -h
-bash 0x04/ImageProcessing.sh -f img/spring.jpeg -q      # 压缩jpg质量
-bash 0x04/ImageProcessing.sh -f img/boy.png -r          # 压缩分辨率
-bash 0x04/ImageProcessing.sh -f img/xumo.svg -t         # 格式转为jpg
-bash 0x04/ImageProcessing.sh -f img/girl.png -p prefix  # 添加前缀
-bash 0x04/ImageProcessing.sh -f img -m                  # 批量添加水印
+bash 0x04/ImageProcessing.sh -f 0x04/img/spring.jpeg -q      # 压缩jpg质量
+bash 0x04/ImageProcessing.sh -f 0x04/img/boy.png -r          # 压缩分辨率
+bash 0x04/ImageProcessing.sh -f 0x04/img/xumo.svg -t         # 格式转为jpg
+bash 0x04/ImageProcessing.sh -f 0x04/img/girl.png -p prefix  # 添加前缀
+bash 0x04/ImageProcessing.sh -f 0x04/img -m                  # 批量添加水印
 printf "-------------------- shellcheck --------------------"
 shellcheck 0x04/ImageProcessing.sh
 
