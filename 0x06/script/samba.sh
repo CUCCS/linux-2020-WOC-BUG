2a0a33e1c476b03a348d2cd7c940246e837cd08f
WOC_BUG <1539777243@qq.com>
2020-06-01 19:58:49 +0800

    change code

--- 0x06/script/samba.sh
+++ 0x06/script/samba.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-sudo su
-apt-get install -y samba
+sudo apt-get install -y samba
 
 smbuser="demoUser"
 smbgroup="demoGroup"
 
 # useradd -M -s /sbin/nologin "$smbuser"
-useradd -M -s $(which nologin) "$smbuser"
+sudo useradd -M -s $(which nologin) "$smbuser"
 echo "$smbuser:password" | chpasswd
 # sed -i.bak 's#^\(smbuser:\)[^:]*\(:.*\)$#\$6$0Nf0oKzZw7$LWRXlj45pDhV/KHEISQhmOLr8hux2tB1DmzPvee0UrbvaOsjbcf3pBAd4RNdzJqdMnsmvC2/FCf7hECsDLhwU/#' /etc/shadow
 (echo password; echo password) | smbpasswd -a "$smbuser"
@@ -27,14 +26,14 @@ force user = "$smbuser"
 force group = "$smbgroup"
 EOT
 
-smbpasswd -e "$smbuser"
-groupadd "$smbgroup"
-usermod -G "$smbgroup" "$smbuser"
-mkdir -p /home/samba/guest/
-mkdir -p /home/samba/demo/
-chgrp -R "$smbgroup" /home/samba/guest/
-chgrp -R "$smbgroup" /home/samba/demo/
-chmod 2775 /home/samba/guest/
-chmod 2770 /home/samba/demo/
+sudo smbpasswd -e "$smbuser"
+sudo groupadd "$smbgroup"
+sudo usermod -G "$smbgroup" "$smbuser"
+sudo mkdir -p /home/samba/guest/
+sudo mkdir -p /home/samba/demo/
+sudo chgrp -R "$smbgroup" /home/samba/guest/
+sudo chgrp -R "$smbgroup" /home/samba/demo/
+sudo chmod 2775 /home/samba/guest/
+sduo chmod 2770 /home/samba/demo/
 
-service smbd restart
+sudo service smbd restart
--- 0x06/script/vsftpd.sh
+++ 0x06/script/vsftpd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get update      # 更新
+sudo apt-get update      # 更新
 
 ## 查看update命令的退出状态码 ##
 if [[ $? -ne 0 ]];then		# 如果不是0就报错
@@ -10,10 +10,10 @@ fi
 
 
 ## 判断目标主机是否已安装vsftpd ##
-command -v vsftpd > /dev/null   # 重定向输出到黑洞文件，可以不显示输出仅判断操作是否正常
+sudo command -v vsftpd > /dev/null   # 重定向输出到黑洞文件，可以不显示输出仅判断操作是否正常
 if [[ $? -ne 0 ]];then
                 # 安装vsftpd
-                apt install vsftpd -y
+                sudo apt install vsftpd -y
                 if [[ $? -ne 0 ]];then
                                 echo "failed to install vsftpd!"
                                 exit
@@ -40,27 +40,27 @@ then
                 mkdir -p "$anon_path"
 fi
 
-chown nobody:nogroup "$anon_path"		# 设置pub文件夹所属用户和组
+sudo chown nobody:nogroup "$anon_path"		# 设置pub文件夹所属用户和组
 echo "vsftpd test file" | sudo tee "${anon_path}/test.txt"      # 将输出重定向到文件test.txt
 
 ## 修改vsftpd.conf ##
 # 开启匿名使用
-sed -i -e "/anonymous_enable=/s/^[#]//g;/anonymous_enable=/s/NO/YES/g" "$conf"
+sudo sed -i -e "/anonymous_enable=/s/^[#]//g;/anonymous_enable=/s/NO/YES/g" "$conf"
 # 不允许匿名用户上传
-sed -i -e "/anon_upload_enable=/s/^[#]//g;/anon_upload_enable=/s/YES/NO/g" "$conf"
+sudo sed -i -e "/anon_upload_enable=/s/^[#]//g;/anon_upload_enable=/s/YES/NO/g" "$conf"
 # 禁止匿名用户的写权限
-sed -i -e "/anon_mkdir_write_enable=/s/^[#]//g;/anon_mkdir_write_enable=/s/YES/NO/g" "$conf"
+sudo sed -i -e "/anon_mkdir_write_enable=/s/^[#]//g;/anon_mkdir_write_enable=/s/YES/NO/g" "$conf"
 
 ## 在/etc/vsftpd.conf文件中搜索关键字，并替换文件中的内容 ##
 # anon_root设为/var/ftp，设置无需密码登录
-grep -q "anon_root=" "$conf" && sed -i -e "#anon_root=#s#^[#]##g;#anon_root=#s#\=.*#=/var/ftp#g" "$conf" || echo "anon_root=/var/ftp" >> "$conf"
-grep -q "no_anon_password=" "$conf" && sed -i -e "/no_anon_password=/s/^[#]//g;/no_anon_password=/s/\=.*/=YES/g" "$conf" || echo "no_anon_password=YES" >> "$conf"
+sudo grep -q "anon_root=" "$conf" && sed -i -e "#anon_root=#s#^[#]##g;#anon_root=#s#\=.*#=/var/ftp#g" "$conf" || echo "anon_root=/var/ftp" >> "$conf"
+sudo grep -q "no_anon_password=" "$conf" && sed -i -e "/no_anon_password=/s/^[#]//g;/no_anon_password=/s/\=.*/=YES/g" "$conf" || echo "no_anon_password=YES" >> "$conf"
 
 ## 设置用户名和密码方式访问的账号 ##
 user="wocbug"
 # 如果passwd文件中没有user用户，则添加用户
 if [[ $(grep -c "^$user:" /etc/passwd) -eq 0 ]];then
-                adduser $user
+                sudo adduser $user
 else
                 echo "${user} is already exited!"
 fi
@@ -74,8 +74,8 @@ else
                 echo "${u_path} is already exited!"
 fi
 
-chown nobody:nogroup "$u_path"	# 设置所有权
-chmod a-w "$u_path"	# 删除写权限
+sudo chown nobody:nogroup "$u_path"	# 设置所有权
+sudo chmod a-w "$u_path"	# 删除写权限
 ls -la "$u_path"	# 验证权限
 
 ## 为用户创建upload目录 ##
@@ -87,43 +87,43 @@ else
                 echo "${u_write_path} is already exited!"
 fi
 
-chown "$user":"$user" "$u_write_path"		# 设置所有权
+sudo chown "$user":"$user" "$u_write_path"		# 设置所有权
 ls -la "$u_path"	# 验证权限
 echo "vsftpd test file" | tee "${u_write_path}/test.txt"
 
 ## 更改相关配置 ##
-sed -i -e "/local_enable=/s/^[#]//g;/local_enbale=/s/NO/YES/g" "$conf"
-sed -i -e "/write_enable=/s/^[#]//g;/^write_enable=/s/NO/YES/g" "$conf"
-sed -i -e "/chroot_local_user=/s/^[#]//g;/chroot_local_user=/s/NO/YES/g" "$conf"	# 限制用户使用权限只限于用户目录
+sudo sed -i -e "/local_enable=/s/^[#]//g;/local_enbale=/s/NO/YES/g" "$conf"
+sudo sed -i -e "/write_enable=/s/^[#]//g;/^write_enable=/s/NO/YES/g" "$conf"
+sudo sed -i -e "/chroot_local_user=/s/^[#]//g;/chroot_local_user=/s/NO/YES/g" "$conf"	# 限制用户使用权限只限于用户目录
 
 ## 设置目标主机用于FTP的范围 ##
 port_min=40000
 port_max=50000
-grep -q "pasv_min_port=" "$conf" && sed -i -e "/pasv_min_port=/s/^[#]//g;/pasv_min_port=/s/\=.*/=${port_min}/g" "$conf" || echo "pasv_min_port=${port_min}" >> "$conf"
-grep -q "pasv_max_port=" "$conf" && sed -i -e "/pasv_max_port=/s/^[#]//g;/pasv_max_port=/s/\=.*/=${port_max}/g" "$conf" || echo "pasv_max_port=${port_max}" >>  "$conf"
+sudo grep -q "pasv_min_port=" "$conf" && sed -i -e "/pasv_min_port=/s/^[#]//g;/pasv_min_port=/s/\=.*/=${port_min}/g" "$conf" || echo "pasv_min_port=${port_min}" >> "$conf"
+sudo grep -q "pasv_max_port=" "$conf" && sed -i -e "/pasv_max_port=/s/^[#]//g;/pasv_max_port=/s/\=.*/=${port_max}/g" "$conf" || echo "pasv_max_port=${port_max}" >>  "$conf"
 
 
 ## 限制用户只有添加到userlist才能访问 ##
-grep -q "userlist_enable=" "$conf" && sed -i -e "/userlist_enable=/s/^[#]//g;/userlist_enable=/s/\=.*/=YES/g" "$conf" || echo "userlist_enable=YES" >> "$conf"
-grep -q "userlist_file=" "$conf" && sed -i -e "#userlist_file=#s#^[#]##g;#userlist_file=#s#\=.*#=/etc/vsftpd.userlist#g" "$conf" || echo "userlist_file=/etc/vsftpd.userlist" >> "$conf"
-grep -q "userlist_deny=" "$conf" && sed -i -e "/userlist_deny=/s/^[#]//g;/userlist_deny=/s/\=.*/=NO/g" "$conf" || echo "userlist_deny=NO" >> "$conf"
+sudo grep -q "userlist_enable=" "$conf" && sed -i -e "/userlist_enable=/s/^[#]//g;/userlist_enable=/s/\=.*/=YES/g" "$conf" || echo "userlist_enable=YES" >> "$conf"
+sudo grep -q "userlist_file=" "$conf" && sed -i -e "#userlist_file=#s#^[#]##g;#userlist_file=#s#\=.*#=/etc/vsftpd.userlist#g" "$conf" || echo "userlist_file=/etc/vsftpd.userlist" >> "$conf"
+sudo grep -q "userlist_deny=" "$conf" && sed -i -e "/userlist_deny=/s/^[#]//g;/userlist_deny=/s/\=.*/=NO/g" "$conf" || echo "userlist_deny=NO" >> "$conf"
 
 ## 将用户添加到userlist ##
-grep -q "$user" /etc/vsftpd.userlist ||  echo "$user" | tee -a /etc/vsftpd.userlist
-grep -q "anonymous" /etc/vsftpd.userlist || echo "anonymous" | tee -a /etc/vsftpd.userlist
+sudo grep -q "$user" /etc/vsftpd.userlist ||  echo "$user" | tee -a /etc/vsftpd.userlist
+sudo grep -q "anonymous" /etc/vsftpd.userlist || echo "anonymous" | tee -a /etc/vsftpd.userlist
 
 ## 只允许白名单用户访问ftp ##
-grep -q "tcp_wrappers=" "$conf" && sed -i -e "/tcp_wrappers=/s/^[#]//g;/tcp_wrappers=/s/NO/YES/g" "$conf" || echo "tcp_wrappers=YES" >> "$conf"
-grep -q "vsftpd:ALL" /etc/hosts.deny || echo "vsftpd:ALL" >> /etc/hosts.deny
-grep -q "vsftpd:192.168.56.101" /etc/hosts.allow || echo "vsftpd:192.168.56.101" >> /etc/hosts.allow
-grep -q "allow_writeable_chroot=" "$conf" && sed -i -e "/allow_writeable_chroot=/s/^[#]//g;/allow_writeable_chroot=/s/NO/YES/g" "$conf" || echo "allow_writeable_chroot=YES" >> "$conf"
+sudo grep -q "tcp_wrappers=" "$conf" && sed -i -e "/tcp_wrappers=/s/^[#]//g;/tcp_wrappers=/s/NO/YES/g" "$conf" || echo "tcp_wrappers=YES" >> "$conf"
+sudo grep -q "vsftpd:ALL" /etc/hosts.deny || echo "vsftpd:ALL" >> /etc/hosts.deny
+sudo grep -q "vsftpd:192.168.56.101" /etc/hosts.allow || echo "vsftpd:192.168.56.101" >> /etc/hosts.allow
+sudo grep -q "allow_writeable_chroot=" "$conf" && sed -i -e "/allow_writeable_chroot=/s/^[#]//g;/allow_writeable_chroot=/s/NO/YES/g" "$conf" || echo "allow_writeable_chroot=YES" >> "$conf"
 
 ## 判断vsftpd进程状态 ##
 if pgrep -x "vsftpd" > /dev/null
 then
-                systemctl restart vsftpd
+                sudo systemctl restart vsftpd
 else
-                systemctl start vsftpd
+                sudo systemctl start vsftpd
 fi
 
 
