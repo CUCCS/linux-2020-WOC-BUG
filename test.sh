2f4cda6283923d2e4ca037f2a44a83d64efb7865
WOC_BUG <1539777243@qq.com>
2020-06-01 20:14:30 +0800

    change code

--- test.sh
+++ test.sh
@@ -1,25 +1,13 @@
 #!/usr/bin/env bash
 
 printf "===============vsftpd.sh==============\n"
-bash 0x06/script/vsftpd.sh
-
-printf "------------------------shellcheck------------------------\n"
 shellcheck 0x06/script/vsftpd.sh
 
 printf "==============nfs_s.sh==============\n"
-bash 0x06/script/nfs_s.sh
-
-printf "------------------------shellcheck------------------------\n"
 shellcheck 0x06/script/nfs_s.sh
 
 printf "==============nfs_c.sh==============\n"
-bash 0x06/script/nfs_c.sh
-
-printf "------------------------shellcheck------------------------\n"
 shellcheck 0x06/script/nfs_c.sh
 
 printf "==============samba.sh==============\n"
-bash 0x06/script/samba.sh
-
-printf "------------------------shellcheck------------------------\n"
 shellcheck 0x06/script/samba.sh
