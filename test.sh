1e32687624da0d9050c72a2ef84f2776d661052c
WOC-BUG <1538777243@qq.com>
2020-06-25 10:25:44 +0800

    change test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+printf "===============vsftpd.sh==============\n"
+shellcheck 0x06/script/vsftpd.sh
 
 printf "==============nfs_s.sh==============\n"
 shellcheck 0x06/script/nfs_s.sh
@@ -6,8 +8,15 @@ shellcheck 0x06/script/nfs_s.sh
 printf "==============nfs_c.sh==============\n"
 shellcheck 0x06/script/nfs_c.sh
 
+printf "===============dhcpd.sh==============\n"
+shellcheck 0x06/script/dhcpd.sh
+
 printf "==============samba.sh==============\n"
 shellcheck 0x06/script/samba.sh
 
-printf "===============vsftpd.sh==============\n"
-shellcheck 0x06/script/vsftpd.sh
+printf "===============dns_s.sh==============\n"
+shellcheck 0x06/script/dns_s.sh
+
+printf "===============dns_c.sh==============\n"
+shellcheck 0x06/script/dns_c.sh
+
