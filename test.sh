afa04611d2e0c156e9a54a1950148ee26f941e3c
WOC-BUG <1538777243@qq.com>
2020-06-25 11:05:25 +0800

    add bash shell

--- test.sh
+++ test.sh
@@ -1,4 +1,27 @@
 #!/usr/bin/env bash
+printf "bash\n"
+printf "===============vsftpd.sh==============\n"
+bash 0x06/script/vsftpd.sh
+
+printf "==============nfs_s.sh==============\n"
+bash 0x06/script/nfs_s.sh
+
+printf "==============nfs_c.sh==============\n"
+bash 0x06/script/nfs_c.sh
+
+printf "===============dhcpd.sh==============\n"
+bash 0x06/script/dhcpd.sh
+
+printf "==============samba.sh==============\n"
+bash 0x06/script/samba.sh
+
+printf "===============dns_s.sh==============\n"
+bash 0x06/script/dns_s.sh
+
+printf "===============dns_c.sh==============\n"
+bash 0x06/script/dns_c.sh
+
+printf "shellcheck\n"
 printf "===============vsftpd.sh==============\n"
 shellcheck 0x06/script/vsftpd.sh
 
